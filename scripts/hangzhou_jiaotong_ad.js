a212dfca76a43720baad8688bb346f0ad1c567af
3kaiu <github@wenn.cc>
2024-07-17 04:10:39 +0800

    1

--- scripts/hangzhou_jiaotong_ad.js
+++ scripts/hangzhou_jiaotong_ad.js
@@ -1,7 +1,8 @@
 let json = JSON.parse($response.body)
 //绿色商城
 if (json.item && json.item.length > 0) {
-	json.item[0].title = '商城'
+	// json.item[0].title = '商城'
+	delete json.item[0]
 }
 //分享应用
 // if (json.items && json.items.length > 3) {
